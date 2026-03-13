122bf13dea00c15ec5c342c9294f6b413d81dea6
Yuhan Zhao <956746759@qq.com>
2026-03-13 08:45:26 +0800

    Finish U6 Congratulations. Next to do is optional.

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://flourishing-unicorn-8134ad.netlify.app/"
+  site: "https://flourishing-unicorn-8134ad.netlify.app/",
+  integrations: [preact()]
 });
