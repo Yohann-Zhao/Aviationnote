db7339bab0d15401d553d6f29f2b96f53a9098ae
Yuhan Zhao <956746759@qq.com>
2026-03-17 22:11:19 +0800

    个人Blog站点上次配置文件是错的。真正原因是markdown文件中图片路径不能有“public”,应该从assets开始。

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -3,10 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portfolio/',  // 这里填子目录，例如：如果你的站点部署在 https://example.com/subdir/，则 base 应该设置为 '/subdir/'。
   i18n: {
     locales: ["zh-CN"],
     defaultLocale: "zh-CN",
   }
 })
-
